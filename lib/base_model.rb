4c047d276336b2600ad4766e389fe1376f0f10e0
Reid Cooper <reid.cooper8@gmail.com>
2019-02-06 08:41:55 -0500

    Adjust client instansiation

--- lib/base_model.rb
+++ lib/base_model.rb
@@ -1,16 +1,20 @@
 module TodoableApi
   class BaseModel
+    def self.client
+      @client ||= TodoableApi.configuration.client_class.new
+    end
 
     attr_reader :id
     attr_reader :raw_json
     attr_reader :errors
 
     def client
-      @client ||= TodoableApi.configuration.client_class.new
+      BaseModel.client
     end
 
     def parse_response(json_body)
       @raw_json = json_body
+      puts @raw_json
     end
   end
 end
